b9e31ae2aab121ca0dff1a15ed218ec902f16e89
James <tingjames2000@outlook.com>
2020-10-13 21:44:27 -0400

    Add solution that adds increasing values

--- CombinationSum3.java
+++ CombinationSum3.java
@@ -1,14 +1,20 @@
+
+//https://leetcode.com/problems/combination-sum-iii/submissions/
 import java.util.ArrayList;
 import java.util.List;
+/* Recrusively create the combinations and check each time to see if a valid combination has been created
+* If yes then add to the list, if not then remove and try the next one
+*
+*/
 
-class combinationSum3 {
-    public List<List<Integer>> combinationSum3Decreasing(int k, int n) {
+class combinationSum3Decreasing {
+    public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
-        helperDecreasing(k, n, n, result, new ArrayList<Integer>());
+        helper(k, n, n, result, new ArrayList<Integer>());
         return result;
     }
 
-    private void helperDecreasing(int k, int n, int start, List<List<Integer>> result, List<Integer> list) {
+    private void helper(int k, int n, int start, List<List<Integer>> result, List<Integer> list) {
         if (k == 0 && n == 0) {
             result.add(new ArrayList<Integer>(list));
         }
@@ -18,7 +24,30 @@ class combinationSum3 {
 
         for (int i = Math.min(9, start); i > 0; i--) {
             list.add(i);
-            helperDecreasing(k - 1, n - i, i - 1, result, list);
+            helper(k - 1, n - i, i - 1, result, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
+
+class combinationSum3Increasing {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(k, n, 0, result, new ArrayList<Integer>());
+        return result;
+    }
+
+    private void helper(int k, int n, int end, List<List<Integer>> result, List<Integer> list) {
+        if (k == list.size() && n == 0) {
+            result.add(new ArrayList<Integer>(list));
+        }
+
+        if (n <= 0 || end > 9)
+            return;
+
+        for (int i = Math.max(1, end); i < 10; i++) {
+            list.add(i);
+            helper(k, n - i, i + 1, result, list);
             list.remove(list.size() - 1);
         }
     }
