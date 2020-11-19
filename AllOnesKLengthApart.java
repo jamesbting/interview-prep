8688b42314422e14bf4b4da1b86642cf7fa1791c
James <tingjames2000@outlook.com>
2020-11-19 13:33:19 -0500

    Added allOnesKLengthApart

---
+++ AllOnesKLengthApart.java
@@ -0,0 +1,16 @@
+public class AllOnesKLengthApart {
+    public boolean kLengthApart(int[] nums, int k) {
+        if (k == 0)
+            return true;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                int j = i + 1;
+                while (j < nums.length && nums[j] == 0)
+                    j++;
+                if (j < nums.length && j - i - 1 < k)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
