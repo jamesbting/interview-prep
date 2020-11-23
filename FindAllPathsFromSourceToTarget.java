3b75f9fb4a8c0f35b8b6c518b292d9759e975349
James <tingjames2000@outlook.com>
2020-11-23 16:46:04 -0500

    Added FinallPathsfromSourceToTarget

---
+++ FindAllPathsFromSourceToTarget.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class FindAllPathsFromSourceToTarget {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(graph, res, path, 0);
+        return res;
+    }
+
+    private void dfs(int[][] graph, List<List<Integer>> res, List<Integer> path, int currNode) {
+        // base case
+
+        if (currNode == graph.length - 1) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int neighbour : graph[currNode]) {
+            path.add(neighbour);
+            dfs(graph, res, path, neighbour);
+            path.remove(path.size() - 1);
+        }
+    }
+}
