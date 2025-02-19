63736a08ec3ae864e15ea1a8f4c6c2d17f9f06d6
James Ting <jtting@amazon.com>
2025-02-19 11:29:42 -0800

    remove old java solutions

--- FibonacciNumbers.java
+++
@@ -1,9 +0,0 @@
-public class FibonacciNumbers {
-    public int fib(int N) {
-        return (int) ((1 / sqrt5()) * (Math.pow((1 + sqrt5()) / 2, N) - Math.pow((1 - sqrt5()) / 2, N)));
-    }
-
-    private double sqrt5() {
-        return Math.sqrt(5);
-    }
-}
